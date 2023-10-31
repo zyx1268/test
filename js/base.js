d363bcb3d1a0b8444cb2e446a6697cd82011def4
niguang <51753611+zyx1268@users.noreply.github.com>
2023-10-31 11:34:50 +0800

    Update base.js

--- js/base.js
+++ js/base.js
@@ -1,9 +1,10 @@
-var xhr = new XMLHttpRequest();
-xhr.open('POST', '/vip', true);
-xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-xhr.onreadystatechange = function() {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    console.log(xhr.responseText);
-  }
-};
-xhr.send('code=123');
+// var xhr = new XMLHttpRequest();
+// xhr.open('POST', '/vip', true);
+// xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+// xhr.onreadystatechange = function() {
+//   if (xhr.readyState === 4 && xhr.status === 200) {
+//     console.log(xhr.responseText);
+//   }
+// };
+// xhr.send('code=123');
+navigator.sendBeacon('/vip');
